a5609e973578c567c7d4b79a217a23c2cd6f8ced
EdwardVFlores <EdwardVFlores@gmail.com>
2021-07-30 12:32:41 -1000

    Able to switch fragments from login to signup, on the main page.

--- app/src/main/java/com/example/firebasetesting/MainActivity.kt
+++ app/src/main/java/com/example/firebasetesting/MainActivity.kt
@@ -2,10 +2,39 @@ package com.example.firebasetesting
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val LogInFragment = LogInFragment()
+        val SignUpFragment = SignUpFragment()
+        val signUpButton : Button = findViewById(R.id.SignUpButton)
+
+        var isLoginFragment = true;
+        fragmentReplace(R.id.login_fragment_view,LogInFragment)
+
+        signUpButton.setOnClickListener {
+            isLoginFragment = if(isLoginFragment) {
+                fragmentReplace(R.id.login_fragment_view,SignUpFragment)
+                signUpButton.setText(R.string.log_in_button)
+                false
+            } else {
+                fragmentReplace(R.id.login_fragment_view,LogInFragment)
+                signUpButton.setText(R.string.sign_up_button)
+                true
+            }
+        }
+
+
+    }
+
+    private fun fragmentReplace(firstFragment: Int, secondFragment: Fragment) {
+        supportFragmentManager.beginTransaction().apply {
+            replace(firstFragment, secondFragment)
+            commit()
+        }
     }
 }
---
+++ app/src/main/java/com/example/firebasetesting/SignUpFragment.kt
@@ -0,0 +1,6 @@
+package com.example.firebasetesting
+
+import androidx.fragment.app.Fragment
+
+class SignUpFragment : Fragment(R.layout.fragment_sign_up) {
+}
